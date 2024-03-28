f403342773e93ad67041a6b804e60d27fa0dd46c
LyaLizhuizhui <LyaLizhuizhui@users.noreply.github.com>
2024-03-28 15:30:51 +0800

    upload project files

--- 9.2_OPP_dynamic_arrays/sketch.js
+++ 9.2_OPP_dynamic_arrays/sketch.js
@@ -6,13 +6,27 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  balls.push(new Ball(mouseX, mouseY, random(-5, 5)))
-  for (let i = 0; i <= balls.length; i++) {
+  background(0);
+  if (mouseIsPressed) {
+    balls.push(new Ball(mouseX, mouseY, random(3, 6)))
+  }
+  for (let i = 0; i < balls.length; i++) {
     let b = balls[i];
     b.move();
+    if (keyIsPressed) {
+      if (key == 'a') {
+        b.speedUp();
+      }
+      if (key == 'd') {
+        b.slowDown();
+      }
+    }
     b.display();
   }
+  if (balls.length > 100) {
+    balls.splice(0, 1);
+  }
+  text(frameRate().toFixed(2), 10, 20)
 }
 
 class Ball {
@@ -20,17 +34,30 @@ class Ball {
     this.x = _X;
     this.y = _Y;
     this.dia = _dia;
-    this.xSpd = random(-1, 2);
-    this.ySpd = random(-2, 4);
+    this.xSpd = random(-2, 2);
+    this.ySpd = random(-2, 2);
   }
   move() {
     this.x += this.xSpd;
     this.y += this.ySpd;
   }
+  slowDown() {
+    this.xSpd *= 0.94;
+    this.ySpd *= 0.94;
+  }
+  speedUp() {
+    this.xSpd *= 1.02;
+    this.ySpd *= 1.02;
+  }
   display() {
     push();
     translate(this.x, this.y);
-    circle(0, 0, this.dia)
+    fill(148, 78, 167, 150);
+    rect(0, 0, this.dia, this.dia);
+    rect(2, 2, this.dia, this.dia);
+    rect(3, 0, this.dia, this.dia);
+    rect(-3, -4, this.dia, this.dia);
+    rect(-2, 3, this.dia, this.dia);
     pop();
 
   }
