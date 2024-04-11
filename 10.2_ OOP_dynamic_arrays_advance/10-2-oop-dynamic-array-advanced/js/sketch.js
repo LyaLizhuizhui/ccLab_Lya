b0c42fa4dea51e7f7cbad57fd5d6ce38e699b592
LyaLizhuizhui <LyaLizhuizhui@users.noreply.github.com>
2024-04-11 14:52:13 +0800

    Update sketch.js

--- 10.2_ OOP_dynamic_arrays_advance/10-2-oop-dynamic-array-advanced/js/sketch.js
+++ 10.2_ OOP_dynamic_arrays_advance/10-2-oop-dynamic-array-advanced/js/sketch.js
@@ -1,30 +1,52 @@
 let particles = [];
 
 function setup() {
-  let canvas = createCanvas(500, 400);
+  let canvas = createCanvas(800, 600);
   canvas.parent("p5-canvas-container");
-  background(220);
   //generate particle
-  particles.push(new Particle(width / 2, height / 2, 30));
+
 }
 
 function draw() {
   background(200);
+  particles.push(new Particle(random(width), height / 2, random(10, 20)));
   for (let i = 0; i < particles.length; i++) {
     let p = particles[i];
+    p.move();
     p.display();
   }
+  while (particles.length > 500) {
+    let index = 0;
+    particles.splice(index, 1);
+  }
+  text(particles.length, 10, 20);
 }
 
 class Particle {
   constructor(_x, _y, _rad) {
     this.x = _x;
     this.y = _y;
+    this.xSpd = 0;
+    this.ySpd = random(-3, 3);
     this.rad = _rad;
+    this.isOutOfCanvas = false;
   }
-  dispay() {
+  move() {
+    this.x += this.xSpd;
+    this.y += this.ySpd;
+  }
+  checkBoundaries() {
+    if (this.y < 0 || this.y > height) {
+      this.isOutOfCanvas = true;
+    }
+  }
+  display() {
     push();
     translate(this.x, this.y);
+
+    if (this.isOutOfCanvas) {
+      fill(255, 0, 0);
+    }
     circle(0, 0, this.rad * 2);
     pop();
   }
